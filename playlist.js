02104323660aa68ab28422e8af1291b52ef464ba
Muhammad Kumail <muhamadkumail@yahoo.com>
2019-12-27 18:54:27 +0400

    Saving feature

--- playlist.js
+++ playlist.js
@@ -1,10 +1,27 @@
-window.onload = init
+window.onload = init;
 
 function init(){
 var button = document.getElementById("addButton");
 button.onclick = handleButtonEvent;
+loadPlaylist();
 }
 
 function handleButtonEvent(){
-    alert("Button was clicked");
+    var textBox = document.getElementById("songTextInput");
+    var inputText = textBox.value;
+
+    var li = document.createElement("li");
+    li.innerHTML = inputText;
+
+    var ul = document.getElementById("playlist");
+    
+
+    if(inputText == ""){
+        alert("Please enter a song name")
+    }else{
+    ul.appendChild(li);
+    save(inputText);
+    }
+
+   
 }
